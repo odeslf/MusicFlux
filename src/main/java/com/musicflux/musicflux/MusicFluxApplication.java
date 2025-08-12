35be1d420cec87e44c4dfdb5ad6b914142e7ef52
Felipe <lipe13silva@hotmail.com>
2025-08-12 13:49:23 -0300

    initial commit

---
+++ src/main/java/com/musicflux/musicflux/MusicFluxApplication.java
@@ -0,0 +1,13 @@
+package com.musicflux.musicflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicFluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicFluxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/musicflux/musicflux/MusicFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicflux.musicflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicFluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
